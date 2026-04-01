1e4f90adf04af927dfdf23013dcad2ed2733f558
denenche <enenchedominion@gmail.com>
2026-04-01 15:37:36 +0100

    dominion work

--- SENTINEL REBORN/personal.go
+++ SENTINEL REBORN/personal.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	
 	"strconv"
 	"strings"
 )
@@ -36,4 +36,39 @@ func Quote(s string) string {
 		}
 	}
 	return result
+}
+
+
+func ToUpper(input string) string {
+    t := "(up)"
+
+    words := strings.Fields(input)
+
+    for i := 0; i < len(words); i++ {
+        w := words[i]
+
+        switch {
+        case w == "(up)":
+
+            if w == t && i > 0 {
+                words[i-1] = strings.ToUpper(string(words[i-1]))
+                words = append(words[:i], words[i+1:]...)
+
+            }
+
+        case strings.HasPrefix(words[i], "(up,"):
+
+            num := strings.TrimRight(words[i+1], ")")
+            n, _ := strconv.Atoi(num)
+
+            for j := i - n; j < i; j++ {
+                words[j] = strings.ToUpper(string(words[j]))
+                words = append(words[:i], words[i+1:]...)
+            }
+
+        }
+
+    }
+    return strings.Join(words, " ")
+
 }
